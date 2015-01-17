d7d20fe4c02412f0025af2c76ab82f53fa374738
mreusen <info@reusenit.nl>
2015-01-17 21:06:47 +0100

    First commit macBook

---
+++ src/nl/reusenit/oca/ch03/selftest/Literally.java
@@ -0,0 +1,15 @@
+package nl.reusenit.oca.ch03.selftest;
+
+public class Literally {
+
+	public static void main(String[] args) {
+
+		int i1 = 1234;
+		int i2 = 1_000;
+		int i3 = 0x12_a;
+		byte b = (byte) 1234;
+		short s = (short) i2;
+		
+	}
+
+}
